c3fb4b1f0ca3e430e3a3019e1f2897e8eaa1a166
Ashik <53247457+victor-aq@users.noreply.github.com>
2022-05-23 12:03:40 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,15 @@
+console.log(document);
+
+console.log(document.domain);
+
+console.log(document.title);
+
+console.log(document.body);
+
+//grabbing the elements
+
+//Get element by id
+
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+headerTitle.innerText = "Hello"
